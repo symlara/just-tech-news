24127bb7d2e263e21a70e7482d1ab9a6be35f3e4
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-14 17:16:09 -0500

    added functions and api calls to get server and database up and running

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+const sequalize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+//turn on routes
+app.use(routes);
+
+//turn on connection to db and server
+sequalize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
