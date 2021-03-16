7ccf5c1cde135cb6e68c0210930ad51399101f0a
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-15 20:28:16 -0500

    integrated comments into app and created new model withe api endpoints

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require ('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -41,4 +42,21 @@ Vote.belongsTo(User, {
   });
 
 
-  module.exports = { User, Post, Vote };
+  Comment.belongsTo(User, {
+      foreignKey: 'user_id'
+  });
+
+  Comment.belongsTo(Post, {
+      foreignKey: 'post_id'
+  });
+
+  User.hasMany(Comment, {
+      foreignKey: 'user_id'
+  });
+
+  Post.hasMany(Comment, {
+      foreignKey: 'post_id'
+  });
+
+
+  module.exports = { User, Post, Vote, Comment };
