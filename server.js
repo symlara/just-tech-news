21255f12ae9b03faaab83691d349eb219e3bdaf3
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-17 17:17:05 -0500

    added handlebars folders and format to app. added nnew homepage to app with post populated.

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequalize = require('./config/connection');
+const path = require('path');
 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+// App initiation
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //turn on routes
 app.use(routes);
