5c62c9c5e28f8ef39b7506da133cf1e2bacc6fc7
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-19 15:01:26 -0500

    added edit post, delete and auth functionality

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,26 @@
+const { Json } = require("sequelize/types/lib/utils");
+
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+        method: `PUT`,
+        body: JSON.stringify({
+            title
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    }else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
