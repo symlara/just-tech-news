c50f9954a0fb9583bbd9ccc573365efc514bb516
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-19 15:29:36 -0500

    adjusted code

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,26 +1,19 @@
-const { Json } = require("sequelize/types/lib/utils");
-
-async function editFormHandler(event) {
+async function deleteFormHandler(event) {
     event.preventDefault();
-
+  
     const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
+      window.location.toString().split('/').length - 1
     ];
     const response = await fetch(`/api/posts/${id}`, {
-        method: `PUT`,
-        body: JSON.stringify({
-            title
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      method: 'DELETE'
     });
-
+  
     if (response.ok) {
-        document.location.replace('/dashboard/');
-    }else {
-        alert(response.statusText);
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
     }
-}
-
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  }
+  
+  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+  
