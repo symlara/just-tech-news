9ef1653e5a8952c25548509bff529cd2eb4a83ac
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-23 16:07:32 -0500

    corrected code on edit post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,19 +1,40 @@
-async function deleteFormHandler(event) {
-    event.preventDefault();
+async function editFormHandler(event) {
+  event.preventDefault();
   
-    const id = window.location.toString().split('/')[
+  const title = document.querySelector('input[name="post-title"]').value;
+  const content = document.querySelector('textarea[name="post-content"]').value;
+
+  // get the post id from the browser address
+  const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
-    ];
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE'
-    });
-  
-    if (response.ok) {
+  ];
+
+  const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          title,
+          content
+      }),
+      headers: {
+          'Content-Type': 'application/json'
+      }
+  });
+
+  if (response.ok) {
+      // if successful return to the dashboard
       document.location.replace('/dashboard/');
-    } else {
+  } else {
       alert(response.statusText);
-    }
   }
-  
-  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
-  
+}
+const id = window.location.toString().split('/')[
+  window.location.toString().split('/').length - 1
+];
+
+const valueResponse = fetch(`/api/posts/${id}`)
+.then(response => response.json())
+.then(data => document.querySelector('textarea[name="post-content"]').value = data.content);
+
+
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
